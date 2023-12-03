6bc08b429d33d278247d80c4a158fda8871c2b5e
Majesty-jun <hkwns01@gmail.com>
2023-12-03 21:01:46 +0900

    Feat: Header 컴포넌트 생성

---
+++ src/app/(components)/Header.tsx
@@ -0,0 +1,9 @@
+import Gnb from "./Gnb";
+
+export default function Header() {
+  return (
+    <header className="bg-black/[.16] flex justify-center absolute left-[50%] translate-x-[-50%] w-full z-[9999]">
+      <Gnb />
+    </header>
+  );
+}
