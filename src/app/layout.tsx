f150dc32d4b1ce919b796f896ab22bc3fb910cc5
Majesty-jun <hkwns01@gmail.com>
2023-12-03 21:05:05 +0900

    Feat: Header, Footer 컴포넌트 layout.tsx에 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Header from "./(components)/Header";
+import Footer from "./(components)/Footer";
 
 export const Pretendard = localFont({
   src: "../../public/fonts/PretendardVariable.woff2",
@@ -18,7 +20,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={Pretendard.className}>{children}</body>
+      <body className={Pretendard.className}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
