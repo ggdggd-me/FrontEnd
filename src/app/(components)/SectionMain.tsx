368225cc39c25aafde28cd8378dc6b3a42b27dfd
Majesty-jun <hkwns01@gmail.com>
2023-12-04 09:09:26 +0900

    Feat: 메인화면 마크업 및 스타일링

---
+++ src/app/(components)/SectionMain.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+export default function SectionMain() {
+  return (
+    <section className="w-full max-w-[1920px] h-[1080px] bg-[url('/assets/main/home/bg-main.jpg')] bg-no-repeat relative">
+      <article className="flex justify-center items-start">
+        <Image
+          className="mt-[380px] z-50"
+          src={"/assets/main/home/icn-fynn.svg"}
+          alt=""
+          width={540}
+          height={540}
+        />
+        <Image
+          className="ml-[-400px] mt-[12px]"
+          src={"/assets/main/home/img-mockup.png"}
+          alt=""
+          width={900}
+          height={900}
+        />
+      </article>
+      <button className="absolute py-[28px] px-[55px] text-[26px] text-white bg-[#242D41] hover:bg-[#61697A] left-2/4 translate-x-[-50%] bottom-[64px] rounded-full">
+        당신을 위한 ‘바른자세 요정’ 만나기 →
+      </button>
+    </section>
+  );
+}
