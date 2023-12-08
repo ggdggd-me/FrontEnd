48d5d3f056ca2f06e4f412d6ca97532f67817d79
Majesty-jun <hkwns01@gmail.com>
2023-12-08 18:58:23 +0900

    Design: tailwind.config.ts 컬러값 지정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,27 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        blue100: "#ECF3F9",
+        blue200: "#AAD1F9",
+        blue300: "#3596FF",
+        blue400: "#6199D1",
+        blue500: "#3F4B65",
+        blue600: "#242D41",
+
+        gray100: "#F9FAFB",
+        gray200: "#DDDDDD",
+        gray300: "#DADADA",
+        gray400: "#A6A6A6",
+        gray500: "#909090",
+        gray600: "#767676",
+        gray700: "#484848",
+        gray800: "#2D2D2D",
+        gray900: "#232323",
+
+        white: "#FFFFFF",
+        black: "#222222",
+      },
     },
   },
   plugins: [],
