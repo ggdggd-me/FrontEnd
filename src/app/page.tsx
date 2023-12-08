f0ce6d8b303955be6507fda60252e8bf1bbdc5d9
Majesty-jun <hkwns01@gmail.com>
2023-12-09 01:57:50 +0900

    Feat: 각 섹션 메인 페이지 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import SectionMain from "./(components)/SectionMain";
+import SectionNotify from "./(components)/SectionNotify";
+import SectionSetting from "./(components)/SectionSetting";
+import SectionTurnOff from "./(components)/SectionTurnOff";
 
 export default function Home() {
   return (
     <main className="w-full flex flex-col justify-center items-center bg-[#ecf3f9] text-[#242D41]">
       <SectionMain />
+      <SectionNotify />
+      <SectionSetting />
+      <SectionTurnOff />
     </main>
   );
 }
