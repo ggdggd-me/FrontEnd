bb95c38dc08dddf99b901a14a819f636144e8d40
Majesty-jun <hkwns01@gmail.com>
2023-12-10 20:16:16 +0900

    Feat: 후기 작성 파트 마크업 및 스타일링

---
+++ src/app/review/(components)/ReviewRegister.tsx
@@ -0,0 +1,16 @@
+export default function ReviewRegister() {
+  return (
+    <article className="w-[340px] h-[270px] bg-blue600 text-white relative rounded-[8px]">
+      <h3 className="text-[32px] pl-[26px] pt-[26px] font-extrabold">
+        꿋꿋을 사용하며
+        <br />
+        느낀 점들을
+        <br />
+        자유롭게 남겨주세요
+      </h3>
+      <button className="w-[96px] h-[28px] absolute bottom-[16px] right-[16px] bg-blue500 text-[12px] rounded-[4px] font-semibold">
+        꿋꿋 후기 쓰기 →
+      </button>
+    </article>
+  );
+}
