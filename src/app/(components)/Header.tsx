14845b954202498fcd31820cd0e3f5d1428add8f
Majesty-jun <hkwns01@gmail.com>
2023-12-13 11:11:30 +0900

    Design: header absolute -> fixed로 스타일 수정

--- src/app/(components)/Header.tsx
+++ src/app/(components)/Header.tsx
@@ -2,7 +2,7 @@ import Gnb from "./Gnb";
 
 export default function Header() {
   return (
-    <header className="bg-black/[.16] flex justify-center absolute left-[50%] translate-x-[-50%] w-full z-[9999]">
+    <header className="bg-black/[.16] flex justify-center fixed left-[50%] translate-x-[-50%] w-full z-[9999]">
       <Gnb />
     </header>
   );
