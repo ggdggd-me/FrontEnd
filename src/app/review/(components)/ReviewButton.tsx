1b08ba08c7e75edccc1b4aac0cb8fd26452af9b1
Majesty-jun <hkwns01@gmail.com>
2023-12-13 11:07:49 +0900

    Feat: 후기 더보기 버튼 마크업 및 스타일링

---
+++ src/app/review/(components)/ReviewButton.tsx
@@ -0,0 +1,9 @@
+export default function ReviewButton() {
+  return (
+    <div className="grow text-center">
+      <button className="text-gray600 font-medium underline mt-[48px]">
+        후기 더보기
+      </button>
+    </div>
+  );
+}
