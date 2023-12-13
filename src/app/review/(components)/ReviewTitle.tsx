596b7e9e500242d651c136a9730f61d821ad0250
Majesty-jun <hkwns01@gmail.com>
2023-12-13 11:08:15 +0900

    Feat: 후기 페이지 제목 마크업 및 스타일링

---
+++ src/app/review/(components)/ReviewTitle.tsx
@@ -0,0 +1,14 @@
+export default function ReviewTitle() {
+  return (
+    <div>
+      <h2 className="text-[28px] font-medium mb-[16px]">
+        꿋프렌즈들이 남겨주신
+        <br />
+        <span className="font-extrabold">생생한 ’바른자세’ 후기</span>
+      </h2>
+      <p className="text-gray600 font-medium">
+        척추요정 핀의 알림을 통해 바른 자세를 되찾은 꿋프렌즈들의 후기
+      </p>
+    </div>
+  );
+}
