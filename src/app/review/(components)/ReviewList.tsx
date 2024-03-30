c27b75058cb8494a8c0ad4c5eb6b75e40fcf674b
Majesty-jun <hkwns01@gmail.com>
2024-03-31 00:00:56 +0900

    Fix: mockup 데이터 import문 삭제

--- src/app/review/(components)/ReviewList.tsx
+++ src/app/review/(components)/ReviewList.tsx
@@ -3,7 +3,6 @@
 import useGetReviews from "@/hooks/useGetReviews";
 import ReviewItem from "./ReviewItem";
 import ReviewRegister from "./ReviewRegister";
-import { mockup } from "./mockupdata";
 
 export default function ReviewList() {
   const { data } = useGetReviews();
