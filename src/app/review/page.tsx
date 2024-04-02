7bb6395f991551bf1761ca7f65a783f4c6b19670
Majesty-jun <hkwns01@gmail.com>
2024-04-02 21:23:49 +0900

    Remove: 후기 페이지 문의 버튼 삭제

--- src/app/review/page.tsx
+++ src/app/review/page.tsx
@@ -1,10 +1,8 @@
 import SectionReview from "./(components)/SectionReview";
-import SectionFaq from "./(components)/SectionFaq";
 
 export default function Review() {
   return (
     <main className="w-full mx-auto max-w-[1920px] bg-[url('/assets/review/bg-review.jpg')] bg-center flex flex-col justify-center items-center gap-[100px] bg-fixed py-[140px]">
-      <SectionFaq />
       <SectionReview />
     </main>
   );
