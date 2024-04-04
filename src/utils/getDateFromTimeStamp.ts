15a00a09e8f96fa78e64f78f604475b734217a1e
Majesty-jun <hkwns01@gmail.com>
2024-04-04 22:39:37 +0900

    Feat: firebase Timestamp 날짜 전환 유틸함수 생성

---
+++ src/utils/getDateFromTimeStamp.ts
@@ -0,0 +1,10 @@
+import { Timestamp } from "firebase/firestore";
+
+export default function getDateFromTimeStamp(date: Timestamp) {
+  const timestamp = date.toDate();
+  const year = timestamp.getFullYear();
+  const month = ("0" + (timestamp.getMonth() + 1)).slice(-2);
+  const day = ("0" + (timestamp.getDate() + 1)).slice(-2);
+
+  return `${year}.${month}.${day}`;
+}
