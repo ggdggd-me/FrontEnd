e6b6bead304be1f16f5295b988d1475e4647441b
Majesty-jun <hkwns01@gmail.com>
2024-04-17 22:46:10 +0900

    Fix: Timestamp 날짜 변환 함수 수정

--- src/utils/getDateFromTimeStamp.ts
+++ src/utils/getDateFromTimeStamp.ts
@@ -4,7 +4,7 @@ export default function getDateFromTimeStamp(date: Timestamp) {
   const timestamp = date.toDate();
   const year = timestamp.getFullYear();
   const month = ("0" + (timestamp.getMonth() + 1)).slice(-2);
-  const day = ("0" + (timestamp.getDate() + 1)).slice(-2);
+  const day = ("0" + timestamp.getDate()).slice(-2);
 
   return `${year}.${month}.${day}`;
 }
