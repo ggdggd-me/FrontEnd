5a5568147eb1f2bf5eb495bc379f9e97cf3f92f2
Majesty-jun <hkwns01@gmail.com>
2024-05-12 22:46:42 +0900

    Feat: 리뷰, QnA 로딩중 상태 표시

--- src/app/qna/(components)/QnaList.tsx
+++ src/app/qna/(components)/QnaList.tsx
@@ -6,15 +6,21 @@ import useGetQnaQuestions from "@/hooks/useGetQnaQuestions";
 import { iQnaQuestion } from "@/interface/interface";
 
 export default function QnaList() {
-  const { data } = useGetQnaQuestions();
+  const { isLoading, data } = useGetQnaQuestions();
 
   return (
-    <ul className="w-[1060px] flex flex-wrap gap-[20px] bg-white p-[40px] mt-[-120px]">
-      {data?.map((question: iQnaQuestion, index) => (
-        <li key={index} className="w-full">
-          <QnaItem question={question} />
-        </li>
-      ))}
-    </ul>
+    <>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <ul className="w-[1060px] flex flex-wrap gap-[20px] bg-white p-[40px] mt-[-120px]">
+          {data?.map((question: iQnaQuestion, index) => (
+            <li key={index} className="w-full">
+              <QnaItem question={question} />
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
   );
 }
--- src/app/review/(components)/ReviewList.tsx
+++ src/app/review/(components)/ReviewList.tsx
@@ -5,18 +5,24 @@ import ReviewItem from "./ReviewItem";
 import ReviewRegister from "./ReviewRegister";
 
 export default function ReviewList() {
-  const { data } = useGetReviews();
+  const { isLoading, data } = useGetReviews();
 
   return (
-    <ul className="w-[1060px] flex flex-wrap gap-[20px]">
-      <li>
-        <ReviewRegister />
-      </li>
-      {data?.map((data, index) => (
-        <li key={index}>
-          <ReviewItem data={data} />
-        </li>
-      ))}
-    </ul>
+    <>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <ul className="w-[1060px] flex flex-wrap gap-[20px]">
+          <li>
+            <ReviewRegister />
+          </li>
+          {data?.map((data, index) => (
+            <li key={index}>
+              <ReviewItem data={data} />
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
   );
 }
